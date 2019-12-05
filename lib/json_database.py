18d2441394473c7b097b36ad5d2838f5329b41ae
Sebastian Schleemilch <basti.schleemilch@onlinehome.de>
2019-12-05 22:27:33 +0100

    lower case JSON

--- lib/json_database.py
+++ lib/json_database.py
@@ -17,20 +17,20 @@ class JSONDatabase():
     def __init__(self, database_name, database_template, database_path='~'):
         self.database_path = JSONDatabase.get_database_path(database_name, database_path)
         if os.path.exists(self.database_path):
-            LOGGER.debug("JSON Database file '%s' already exists. Loading content now.", self.database_path)
+            LOGGER.debug("json database file '%s' already exists. Loading content now.", self.database_path)
             with open(self.database_path) as f:
                 self.data = json.load(f)
         else:
-            LOGGER.debug("JSON Database file '%s' not yet existing. Writing template to file.", self.database_path)
+            LOGGER.debug("json database file '%s' not yet existing. Writing template to file.", self.database_path)
             self.data = database_template
             with open(self.database_path, 'w') as database:
                 json.dump(self.data, database)
 
     def save(self):
-        LOGGER.debug("Saving JSON database '%s' to disk.", self.database_path)
+        LOGGER.debug("Saving json database '%s' to disk.", self.database_path)
         with open(self.database_path, 'w') as database:
             json.dump(self.data, database, indent=4)
 
     def delete(self):
-        LOGGER.warning("Deleting JSON database '%s'", self.database_path)
+        LOGGER.warning("Deleting json database '%s'", self.database_path)
         os.remove(self.database_path)
